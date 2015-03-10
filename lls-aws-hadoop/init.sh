270a1520c4c6b4e781e12289b9b8e2ad4cc30a66
Guodong Wang <gordon@liulishuo.com>
2015-03-10 15:04:29 +0800

    change hadoop_home as /root/hadoop

--- lls-aws-hadoop/init.sh
+++ lls-aws-hadoop/init.sh
@@ -2,7 +2,7 @@
 # @Author: Guodong Wang
 # @Date:   2015-02-27 17:58:04
 # @Last Modified by:   Guodong Wang
-# @Last Modified time: 2015-03-10 11:09:17
+# @Last Modified time: 2015-03-10 15:03:41
 
 function die()
 {
@@ -16,8 +16,9 @@ cd $download_dir
 rm -rf hadoop_conf
 wget --reject "index.html*" -nH -r --no-parent http://ip-172-31-7-86/hadoop_conf/
 
-hadoop_conf_dir=/etc/hadoop/conf
+hadoop_conf_dir=/root/hadoop/etc/hadoop
 rm -rf $hadoop_conf_dir
 mkdir $hadoop_conf_dir || die "fail to create $hadoop_conf_dir"
 
 mv hadoop_conf/* $hadoop_conf_dir
+rmdir hadoop_conf
