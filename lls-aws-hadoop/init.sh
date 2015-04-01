79282920a7bc05b9c7613ce81f8ee8003ce57889
Guodong Wang <gordon@liulishuo.com>
2015-04-01 20:23:00 +0800

    checkout aws hadoop conf from gitlab

--- lls-aws-hadoop/init.sh
+++ lls-aws-hadoop/init.sh
@@ -2,7 +2,7 @@
 # @Author: Guodong Wang
 # @Date:   2015-02-27 17:58:04
 # @Last Modified by:   Guodong Wang
-# @Last Modified time: 2015-03-10 15:03:41
+# @Last Modified time: 2015-04-01 20:04:02
 
 function die()
 {
@@ -13,12 +13,12 @@ function die()
 download_dir=/root/aws-hadoop/
 
 cd $download_dir
-rm -rf hadoop_conf
-wget --reject "index.html*" -nH -r --no-parent http://ip-172-31-7-86/hadoop_conf/
+rm -rf aws-hadoop-conf
+git clone -b s3a git@git.llsapp.com:data-pipeline/aws-hadoop-conf.git
+
 
 hadoop_conf_dir=/root/hadoop/etc/hadoop
 rm -rf $hadoop_conf_dir
 mkdir $hadoop_conf_dir || die "fail to create $hadoop_conf_dir"
 
-mv hadoop_conf/* $hadoop_conf_dir
-rmdir hadoop_conf
+mv aws-hadoop-conf/hadoop_conf/* $hadoop_conf_dir
