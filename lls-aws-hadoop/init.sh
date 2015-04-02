a7648fc0cf701071b3c9cf366ea1b085d63a11c4
Guodong Wang <gordon@liulishuo.com>
2015-04-02 20:57:38 +0800

    fix bug in lls hadoop install

--- lls-aws-hadoop/init.sh
+++ lls-aws-hadoop/init.sh
@@ -2,7 +2,7 @@
 # @Author: Guodong Wang
 # @Date:   2015-02-27 17:58:04
 # @Last Modified by:   Guodong Wang
-# @Last Modified time: 2015-04-01 20:04:02
+# @Last Modified time: 2015-04-02 20:57:07
 
 function die()
 {
@@ -12,6 +12,9 @@ function die()
 
 download_dir=/root/aws-hadoop/
 
+rm -rf $download_dir
+mkdir -p $download_dir
+echo "change working dir to $download_dir"
 cd $download_dir
 rm -rf aws-hadoop-conf
 git clone -b s3a git@git.llsapp.com:data-pipeline/aws-hadoop-conf.git
@@ -21,4 +24,4 @@ hadoop_conf_dir=/root/hadoop/etc/hadoop
 rm -rf $hadoop_conf_dir
 mkdir $hadoop_conf_dir || die "fail to create $hadoop_conf_dir"
 
-mv aws-hadoop-conf/hadoop_conf/* $hadoop_conf_dir
+mv aws-hadoop-conf/hadoop-conf/* $hadoop_conf_dir
