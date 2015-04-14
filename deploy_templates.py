ce93b7590fa9a2473e895a8a7e917b52e9c28c6d
Guodong Wang <gordon@liulishuo.com>
2015-04-14 20:26:31 +0800

    set virtual cores to 1.5*physical cores

--- deploy_templates.py
+++ deploy_templates.py
@@ -64,7 +64,7 @@ tachyon_mb = spark_mb
 
 worker_instances = int(os.getenv("SPARK_WORKER_INSTANCES", 1))
 # Distribute equally cpu cores among worker instances
-worker_cores = max(slave_cpus / worker_instances, 1)
+worker_cores = max(int(1.5 * slave_cpus / worker_instances), 1)
 
 template_vars = {
   "master_list": os.getenv("MASTERS"),
