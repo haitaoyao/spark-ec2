f39a176b9cc44ed0db197c62901d96080e70515a
Haitao Yao <yao.erix@gmail.com>
2015-07-16 14:29:56 +0800

    change to download from s3

--- lls-aws-hadoop/init.sh
+++ lls-aws-hadoop/init.sh
@@ -17,9 +17,10 @@ mkdir -p $download_dir
 echo "change working dir to $download_dir"
 cd $download_dir
 rm -rf aws-hadoop-conf
-git clone -b master git@git.llsapp.com:data-pipeline/aws-hadoop-conf.git
-
-
+#git clone -b master git@git.llsapp.com:data-pipeline/aws-hadoop-conf.git
+aws s3 --region cn-north-1 cp s3://warehouse/hadoop-conf/aws-hadoop-conf.tar.gz .
+tar xvf aws-hadoop-conf.tar.gz
+echo 'config downloaded '
 hadoop_conf_dir=/root/hadoop/etc/hadoop
 rm -rf $hadoop_conf_dir
 mkdir $hadoop_conf_dir || die "fail to create $hadoop_conf_dir"
